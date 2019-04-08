07879003c8f9e0508182c02267c4720588c6408e
tsurumiii <tsurumi0302@gmail.com>
2019-04-08 19:15:53 +0900

    add library

---
+++ lib/models/suggestion.dart
@@ -0,0 +1,39 @@
+import 'dart:collection';
+import 'package:english_words/english_words.dart';
+
+final Suggestion suggetion = Suggestion();
+
+class Suggestion {
+  final List<WordPair> _suggestedWords = <WordPair>[];
+
+  Suggestion();
+
+  Suggestion.clone(Suggestion suggestion) {
+    _suggestedWords.addAll(suggestion._suggestedWords);
+  }
+
+  int get suggestionCount => _suggestedWords.length;
+
+  UnmodifiableListView<WordPair> get suggestedWords =>
+      UnmodifiableListView(_suggestedWords);
+
+  void add(WordPair wordPair) {
+    _updateCount(wordPair);
+  }
+
+  void addMulti(List<WordPair> wordPairs) {
+    for (int i = 0; i < wordPairs.length; ++i) {
+      _updateCount(wordPairs[i]);
+    }
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '$suggestedWords';
+  }
+
+  void _updateCount(WordPair wordPair) {
+    _suggestedWords.add(wordPair);
+  }
+}
