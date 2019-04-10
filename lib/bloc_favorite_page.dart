96253d32c884d35d03a6ec2d43f772e27b7155ea
tsurumiii <tsurumi0302@gmail.com>
2019-04-10 21:03:15 +0900

    add widget Favorite page [bloc_favorite_page.dart]

---
+++ lib/bloc_favorite_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import './models/word_item.dart';
+import './word_provider.dart';
+
+class BlocFavoritePage extends StatelessWidget {
+  BlocFavoritePage();
+  @override
+  Widget build(BuildContext context) {
+    final word = WordProvider.of(context);
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ファボ'),
+      ),
+      body: StreamBuilder<List<WordItem>>(
+        stream: word.items,
+        builder: (context, snapshot) {
+          if (snapshot.data == null || snapshot.data.isEmpty) {
+            return Container(
+              height: height,
+              width: width,
+              child: Container(
+                alignment: Alignment.center,
+                child: Text('まだお気に入りに追加されてません。'),
+              ),
+            );
+          }
+          final tiles = snapshot.data.map((f) {
+            return ListTile(
+              title: Text(f.name),
+            );
+          });
+
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return ListView(
+            children: divided,
+          );
+        },
+      ),
+    );
+  }
+}
